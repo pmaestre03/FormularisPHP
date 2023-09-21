67fa40b6dd13f3de02197086bfc88ae370f8041e
pmaestre03 <maestrep15@gmail.com>
2023-09-21 20:19:40 +0200

    SECOND commit

--- login.php
+++ login.php
@@ -1,7 +1,30 @@
 <h2>Login</h2>
 
-<form>
-    <input type="text" name="user">
+<form method="post">
+    <input type="text" name="user" placeholder="user">
     <br>
-    <input type="password" name=password>
+    <input type="password" name="passwd" placeholder="password">
+    <br>
+    <input type="submit">
 </form>
+
+<?php
+$users = [
+    "pepe" => "1234",
+    "pau" => "5678",
+    "pepa" => "9876"
+    
+];
+var_dump($users);
+if (isset($_POST["user"])) {
+    if ($users[0]==$_POST["user"] && $users[1]==$_POST["passwd"]) {
+        echo "Welcome, " . $_POST["user"];
+    }
+    else {
+        echo "User or password invalid";
+    }
+}
+
+
+
+?>
